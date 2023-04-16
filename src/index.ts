eb677d134aa92ecd289c256689b84177f97c0474
Dark Hole <dark.hole1@yandex.ru>
2023-04-16 16:34:14 +0300

    Minor fix

--- src/index.ts
+++ src/index.ts
@@ -211,7 +211,7 @@ bot.on('callback_query:data', async ctx => {
     await handler(ctx)
 })
 
-bot.command('observed', ctx => ctx.reply(`Всё что я наблюдаю: ${ANIMES.list().map(anime => `* ${anime.name}`).join('\n')}`, {
+bot.command('observed', ctx => ctx.reply(`Всё что я наблюдаю:\n${ANIMES.list().map(anime => `* ${anime.name}`).join('\n')}`, {
     reply_to_message_id: ctx.message?.message_id
 }))
 
