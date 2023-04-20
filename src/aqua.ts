b5b60a870aea2fd4dcf773861173804e71e8609c
Dark Hole <dark.hole1@yandex.ru>
2023-04-20 16:18:06 +0300

    Add testing pagineted sticker

--- src/aqua.ts
+++ src/aqua.ts
@@ -13,11 +13,26 @@ bot.command(['start', 'hastokuwatched', 'recommend', 'observed', 'addsticker'], 
     reply_to_message_id: ctx.msg.message_id
 }))
 
-bot.command('inspect').filter(
-    ctx => ctx.from?.id == DARK_HOLE,
+const admin = bot.filter(ctx => ctx.from?.id == DARK_HOLE)
+
+admin.command(
+    'inspect',
     ctx => ctx.replyFmt(fmt`${pre(JSON.stringify(ctx.msg.reply_to_message, null, 2), 'json')}`, {
         reply_to_message_id: ctx.msg.message_id
     })
+)
+
+admin.command(
+    'paginated',
+    async ctx => {
+        const file_id = ctx.msg.sticker?.file_id
+        if(!file_id) {
+            return
+        }
+        await ctx.replyWithSticker(file_id, {
+            reply_to_message_id: ctx.msg.message_id
+        })
+    }
 )
 
 bot.start()
