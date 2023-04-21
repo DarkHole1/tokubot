54df3cc6cae9b9aa0641d45f4c5935f5c2d3e9cf
Dark Hole <dark.hole1@yandex.ru>
2023-04-21 21:24:16 +0300

    Add helper method for filtering admins

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import { Context } from "grammy";
+import { ADMINS } from "./constants";
+
 export function throttle<Args extends any[], R>(time: number, f: (...args: Args) => R): (...t: Args) => R | void {
     let lastTime: number = 0;
     return function (...args: Args) {
@@ -17,4 +20,16 @@ export function choice<T>(arr: T[]) {
 export function randomString() {
     // I know, this's not secure, but who cares? 
     return (Math.random() + 1).toString(36).substring(7)
+}
+
+export function isAdmin(ctx: Context): boolean {
+    if(ctx.from && ADMINS.includes(ctx.from.id)) {
+        return true
+    }
+
+    if(ctx.senderChat && ADMINS.includes(ctx.senderChat.id)) {
+        return true
+    }
+
+    return false 
 }
