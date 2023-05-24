c88626251abef0ce868ebde8d5c87694558ac251
Dark Hole <dark.hole1@yandex.ru>
2023-05-24 22:27:58 +0300

    Moved greeting to another module

--- src/static.ts
+++ src/static.ts
@@ -1,4 +1,4 @@
-import { code, fmt } from "@grammyjs/parse-mode"
+import { code, fmt, linkMessage } from "@grammyjs/parse-mode"
 
 export const help = fmt
 `Этот бот умеет:
@@ -6,3 +6,8 @@ export const help = fmt
 * Делать рекомендации на основе списка Току. Попробуйте скопировать и отправить ${code(`/recommend`)}
 
 Вот и всё! Связаться с разработчиком: @darkhole1`
+
+export const greeting = fmt
+`Nyanpasu~
+
+Приходи, присаживайся, читай ${linkMessage(`правила`, 1311183194, 11885)} и список рекомендуемых аниме к ${linkMessage(`просмотру`, 1311183194, 84785)}`
