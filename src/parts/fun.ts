1cd9491f78ec8c0f01ba899aa53b063974be1668
Dark Hole <dark.hole1@yandex.ru>
2023-05-25 00:27:33 +0300

    Add achivements

--- src/parts/fun.ts
+++ src/parts/fun.ts
@@ -19,6 +19,7 @@ fun.on(':sticker').filter(ctx => ctx.msg.chat.id == TOKU_CHAT, async ctx => {
     let drink: string
     let count: number
     let emoji: string
+    let achivement = ''
 
     if(!TEA_STICKERS.concat(COFFEE_STICKERS).includes(sticker)) {
         return
@@ -29,15 +30,70 @@ fun.on(':sticker').filter(ctx => ctx.msg.chat.id == TOKU_CHAT, async ctx => {
         drink = 'чя'
         count = drinksCounters.tea
         emoji = '🍵'
+        switch(count) {
+            case 1:
+                achivement = 'Чай буш?'
+                break
+            case 28:
+                achivement = 'Вы выпили ведро чя :0'
+                break
+            case 75:
+                achivement = 'Хватит на аквариум. С чаем.'
+                break
+            case 100:
+                achivement = 'Твой чй пронзит небеса!'
+                break
+            case 150:
+                achivement = '"Я должен был пить много чая, ибо без него не мог работать. Чай высвобождает те возможности, которые дремлют в глубине моей души." Лев Толстой'
+                break
+            case 200:
+                achivement = 'Пора заводить чаегонный аппарт на 60 литров.'
+            case 250:
+                achivement = 'Теперь вы полностью состоите из чая.'
+                break
+            case 300:
+                achivement = 'Хватит чтобы наполнить стиральную машину. Чаем, конечно же.'
+            case 1337:
+                achivement = '31337 t34'
+                break
+        }
     } else {
         drinksCounters.coffee += 1
         drink = 'кфе'
         count = drinksCounters.coffee
         emoji = '☕️'
+        switch(count) {
+            case 1:
+                achivement = 'На этом ты не остановишься. Так мне сказал мой побочный эффект.'
+                break
+            case 50:
+                achivement = 'What is this, a coffee episode?'
+                break
+            case 75:
+                achivement = 'Ничто на свете не даётся даром. Даже кофе.'
+            case 100:
+                achivement = 'Ты выпил 100 чашек кофе? Как мило'
+                break
+            case 150:
+                achivement = 'Во-первых, у тебя лишь сто пятьдесят чашек кофе. Во-вторых, делай, как я говорю, и не вздумай пить меньше кофе. В-третьих, что бы ни было в прошлом, чашки чая тебя не касаются'
+                break
+            case 200:
+                achivement = 'Сломай систему, посмотри аниме, где милые девочки пьют кфе.'
+                break
+            case 250:
+                achivement = 'Wonder Coffee Priority'
+                break
+            case 300:
+                achivement = 'Half human, half coffee, completely awesome.'
+                break
+            case 1300:
+                achivement = 'Этого достаточно чтобы наполнить ванну!'
+                break
+        }
     }
 
     await drinksCounters.toFile('data/drinks.json')
-    await ctx.reply(`Приятного! Попили ${drink} ${count} раз ${emoji}`, {
+    await ctx.reply(`Приятного! Попили ${drink} ${count} раз ${emoji}\n${achivement}`, {
         reply_to_message_id: ctx.msg.message_id
     })
 })
