bbb75c7e53588d5b16d80943bc6c38d03c98de0e
Dark Hole <dark.hole1@yandex.ru>
2023-06-15 22:00:57 +0300

    Add erai token to config

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,14 @@ import * as dotenv from 'dotenv'
 import { z } from 'zod'
 
 const RawConfig = z.object({
-    TOKEN: z.string().min(1)
+    TOKEN: z.string().min(1),
+    ERAI_TOKEN: z.string().min(1)
 })
 type RawConfig = z.infer<typeof RawConfig>
 
 export class Config implements RawConfig {
     TOKEN: string
+    ERAI_TOKEN: string
 
     constructor() {
         const config = dotenv.config()
@@ -16,5 +18,6 @@ export class Config implements RawConfig {
         }
         const parsed = RawConfig.parse(config.parsed)
         this.TOKEN = parsed.TOKEN
+        this.ERAI_TOKEN = parsed.ERAI_TOKEN
     }
 }
