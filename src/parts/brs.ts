cf3e2622359ff7f126b2e2600886e439399f3154
Dark Hole <dark.hole1@yandex.ru>
2023-06-26 15:35:20 +0300

    Minor update

--- src/parts/brs.ts
+++ src/parts/brs.ts
@@ -31,7 +31,7 @@ export function brs(bot: Bot<ParseModeFlavor<Context>>) {
             }
             brsFile.queue.push(photo.slice(-1)[0].file_id)
             await writeFile('data/brs.json', JSON.stringify(brsFile))
-            await ctx.reply('Успешно добавлено', { reply_to_message_id: ctx.msg.message_id })
+            await ctx.reply(`Успешно добавлено на день ${brsFile.days + brsFile.queue.length}`, { reply_to_message_id: ctx.msg.message_id })
         }
     )
 }
