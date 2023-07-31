4a54037f1039370fa5a58ada81f8dc9dd230f8f0
Dark Hole <dark.hole1@yandex.ru>
2023-07-31 20:41:42 +0300

    Fix error

--- src/parts/voting2.ts
+++ src/parts/voting2.ts
@@ -25,8 +25,12 @@ voting2.command('rating', async ctx => {
     const unique = votes.unique()
     const count = votes.count()
     const mapped = count.map(anime => `* ${anime.name} / ${anime.russian}:\n${Object.entries(anime.votes).filter(([_, v]) => v > 0).map(([k, v]) => `  ${k}: ${v}`).join('\n')}`)
-    await ctx.reply(`Проголосовало ${unique} человек\nРезультаты:\n${mapped.slice(0, 25).join('\n')}`)
-    await ctx.reply(mapped.slice(25).join('\n'))
+    try {
+        await ctx.reply(`Проголосовало ${unique} человек\nРезультаты:\n${mapped.slice(0, 25).join('\n')}`)
+        await ctx.reply(mapped.slice(25).join('\n'))
+    } catch(e) {
+        await ctx.reply(`Error: ${e}`)
+    }
 })
 
 voting2.callbackQuery('voting:start', async ctx => {
