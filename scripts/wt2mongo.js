06e0ccc12bbb1244c87d5791addcb4a2403640d4
Dark Hole <dark.hole1@yandex.ru>
2023-10-09 23:07:06 +0300

    Add more true js

--- scripts/wt2mongo.js
+++ scripts/wt2mongo.js
@@ -1,104 +1,41 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_extra_1 = require("fs-extra");
-var mongoose_1 = require("mongoose");
-var config_1 = require("../src/config");
-var brs_1 = require("../src/models/brs");
-var counters_1 = require("../src/models/counters");
-var everyday_post_1 = require("../src/models/everyday-post");
-var debug_1 = require("debug");
-var log = (0, debug_1.debug)('script:wt2mongo');
+const fs_extra_1 = require("fs-extra");
+const mongoose_1 = require("mongoose");
+const config_1 = require("../src/config");
+const brs_1 = require("../src/models/brs");
+const counters_1 = require("../src/models/counters");
+const everyday_post_1 = require("../src/models/everyday-post");
+const debug_1 = require("debug");
+const log = (0, debug_1.debug)('script:wt2mongo');
 log('Loading config & wt file');
-var config = new config_1.Config();
-var wtFile = brs_1.RawBRS.parse(JSON.parse((0, fs_extra_1.readFileSync)('data/world-trigger.json', { encoding: 'utf-8' })));
+const config = new config_1.Config();
+const wtFile = brs_1.RawBRS.parse(JSON.parse((0, fs_extra_1.readFileSync)('data/world-trigger.json', { encoding: 'utf-8' })));
 log('Loaded');
-void (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var counters, _i, _a, _b, i, fileId, post;
-    return __generator(this, function (_c) {
-        switch (_c.label) {
-            case 0:
-                log('Starting connecting to mongo...');
-                return [4 /*yield*/, mongoose_1.default.connect(config.MONGODB_URI)];
-            case 1:
-                _c.sent();
-                log('Connected to mongodb');
-                return [4 /*yield*/, counters_1.CountersModel.findOne()];
-            case 2:
-                counters = _c.sent();
-                if (!!counters) return [3 /*break*/, 4];
-                log('Counter not found');
-                return [4 /*yield*/, counters_1.CountersModel.create({ worldTriggerDays: wtFile.days })];
-            case 3:
-                _c.sent();
-                return [3 /*break*/, 6];
-            case 4:
-                log('Counter found, changing...');
-                counters.worldTriggerDays = wtFile.days;
-                return [4 /*yield*/, counters.save()];
-            case 5:
-                _c.sent();
-                log('Counter changed');
-                _c.label = 6;
-            case 6:
-                log('Starting converting %d file ids to posts in mongo', wtFile.queue.length);
-                _i = 0, _a = wtFile.queue.entries();
-                _c.label = 7;
-            case 7:
-                if (!(_i < _a.length)) return [3 /*break*/, 10];
-                _b = _a[_i], i = _b[0], fileId = _b[1];
-                log('Converting %d post...', i + 1);
-                post = new everyday_post_1.EverydayPostModel({
-                    type: 'world trigger',
-                    fileId: fileId
-                });
-                return [4 /*yield*/, post.save()];
-            case 8:
-                _c.sent();
-                log('Converted!');
-                _c.label = 9;
-            case 9:
-                _i++;
-                return [3 /*break*/, 7];
-            case 10: return [4 /*yield*/, mongoose_1.default.disconnect()];
-            case 11:
-                _c.sent();
-                return [2 /*return*/];
-        }
-    });
-}); })().catch(console.log);
+void (async () => {
+    log('Starting connecting to mongo...');
+    await mongoose_1.default.connect(config.MONGODB_URI);
+    log('Connected to mongodb');
+    const counters = await counters_1.CountersModel.findOne();
+    if (!counters) {
+        log('Counter not found');
+        await counters_1.CountersModel.create({ worldTriggerDays: wtFile.days });
+    }
+    else {
+        log('Counter found, changing...');
+        counters.worldTriggerDays = wtFile.days;
+        await counters.save();
+        log('Counter changed');
+    }
+    log('Starting converting %d file ids to posts in mongo', wtFile.queue.length);
+    for (const [i, fileId] of wtFile.queue.entries()) {
+        log('Converting %d post...', i + 1);
+        const post = new everyday_post_1.EverydayPostModel({
+            type: 'world trigger',
+            fileId
+        });
+        await post.save();
+        log('Converted!');
+    }
+    await mongoose_1.default.disconnect();
+})().catch(console.log);
--- src/config.js
+++ src/config.js
@@ -1,28 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Config = void 0;
-var dotenv = require("dotenv");
-var zod_1 = require("zod");
-var RawConfig = zod_1.z.object({
+const dotenv = require("dotenv");
+const zod_1 = require("zod");
+const RawConfig = zod_1.z.object({
     TOKEN: zod_1.z.string().min(1),
     ERAI_TOKEN: zod_1.z.string().min(1),
     SAGIRI_TOKEN: zod_1.z.string().min(1),
     VK_SERVICE_KEY: zod_1.z.string().min(1),
     MONGODB_URI: zod_1.z.string().min(1)
 });
-var Config = /** @class */ (function () {
-    function Config() {
-        var config = dotenv.config();
+class Config {
+    constructor() {
+        const config = dotenv.config();
         if (!('parsed' in config) || !config.parsed) {
             throw new Error('No config found');
         }
-        var parsed = RawConfig.parse(config.parsed);
+        const parsed = RawConfig.parse(config.parsed);
         this.TOKEN = parsed.TOKEN;
         this.ERAI_TOKEN = parsed.ERAI_TOKEN;
         this.SAGIRI_TOKEN = parsed.SAGIRI_TOKEN;
         this.VK_SERVICE_KEY = parsed.VK_SERVICE_KEY;
         this.MONGODB_URI = parsed.MONGODB_URI;
     }
-    return Config;
-}());
+}
 exports.Config = Config;
