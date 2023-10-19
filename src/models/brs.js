ca2ea10a18ff713ebdbe49d36aed3890a9305007
Dark Hole <dark.hole1@yandex.ru>
2023-10-19 21:56:09 +0300

    Removed compiled sources

--- src/models/brs.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RawBRS = void 0;
-const zod_1 = require("zod");
-exports.RawBRS = zod_1.z.object({
-    days: zod_1.z.number().int(),
-    queue: zod_1.z.array(zod_1.z.string())
-});
