bf94e29184ec5e0d66d4b0fa01844e84d9a7642f
Dark Hole <dark.hole1@yandex.ru>
2023-10-31 20:09:39 +0300

    Updated script

--- scripts/fetchSeasonalAnimes.ts
+++ scripts/fetchSeasonalAnimes.ts
@@ -1,7 +1,8 @@
 import { outputFile } from "fs-extra"
-import { AnimeKind, API } from "shikimori"
+import { AnimeKind, AnimesGetParams, API } from "shikimori"
 
 const shikimori = new API({
+    baseURL: 'https://shikimori.one/api',
     axios: {
         headers: {
             'Accept-Encoding': '*'
@@ -9,17 +10,19 @@ const shikimori = new API({
     }
 })
 
-const SEASON = 'fall_2023'
-const VOTES_FILE = 'data/votes3.json'
+const SEASON = 'summer_2023'
+const VOTES_FILE = 'data/votes4.json'
 const KINDS = 'tv,ona,ova' as AnimeKind
 
+const BASIC_CONFIG: AnimesGetParams = {
+    season: SEASON,
+    order: 'name',
+    limit: 50,
+    kind: KINDS
+}
+
 void async function () {
-    let animes = await shikimori.animes.get({
-        season: SEASON,
-        order: 'name',
-        limit: 50,
-        kind: KINDS
-    })
+    let animes = await shikimori.animes.get(BASIC_CONFIG)
     let page = 2
     while (true) {
         await sleep(20_000)
@@ -41,7 +44,7 @@ void async function () {
             name: anime.name,
             russian: anime.russian,
             url: anime.url,
-            votes: []
+            votes: {}
         }
     })
     outputFile(VOTES_FILE, JSON.stringify(res))
