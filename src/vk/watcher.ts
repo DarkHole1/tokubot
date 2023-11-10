65b6f1e6b4e4bd0fe92ce01908b53700378a4d7e
Dark Hole <dark.hole1@yandex.ru>
2023-11-11 01:19:21 +0300

    Add updating date

--- src/vk/watcher.ts
+++ src/vk/watcher.ts
@@ -29,18 +29,21 @@ export async function watchGroups(token: string, groupIds: number[], cb: (newPos
     setInterval(async () => {
         try {
             let newPosts = [] as WallWallpostFull[]
-            for(const [i, group] of groupIds.entries()) {
+            for (const [i, group] of groupIds.entries()) {
                 const res = await vk.api.wall.get({
                     owner_id: group,
                     count: 10
                 })
                 newPosts = newPosts.concat(res.items.filter(post => post.date && post.date > lastDates[i]))
+                if (res.items[0].date) {
+                    lastDates[i] = res.items[0].date
+                }
                 sleep(fullConfig.wait)
             }
-            if(newPosts.length > 0) {
+            if (newPosts.length > 0) {
                 cb(newPosts)
             }
-        } catch(e) {
+        } catch (e) {
             console.error('Error during parsing vk feed %o', e)
         }
     }, fullConfig.every * 1000)
