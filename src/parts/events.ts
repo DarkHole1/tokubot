4d0ed5b63adeae29428d2772db2717c4ed8185a7
Dark Hole <dark.hole1@yandex.ru>
2024-01-14 14:44:32 +0300

    Add row command

--- src/parts/events.ts
+++ src/parts/events.ts
@@ -1,8 +1,11 @@
-import { Composer } from 'grammy'
+import { Composer, InlineKeyboard, Keyboard } from 'grammy'
 import { Cache } from '../models/cache'
+import { autoQuote } from '@roziscoding/grammy-autoquote'
+import { EventModel } from '../models/events'
 
 export const events = (cache: Cache) => {
     const events = new Composer
+    const quoted = events.use(autoQuote)
 
     events.callbackQuery(/approve:.+/, async ctx => {
         // TODO
@@ -12,11 +15,36 @@ export const events = (cache: Cache) => {
         // TODO
     })
 
-    events.command('row', async ctx => {
-        // TODO
+    quoted.command('row', async ctx => {
+        const reply = ctx.msg.reply_to_message
+        if(!reply || !reply.photo) {
+            await ctx.reply('Ответьте на сообщение с картинкой, которую вы хотите поставить на аватарку')
+            return
+        }
+
+        if(reply.caption) {
+            await ctx.reply('Название чата можно поменять только при сборе всего бинго')
+            return
+        }
+
+        const event = new EventModel({
+            approved: false,
+            duration: 1,
+            pic: reply.photo.at(-1)
+        })
+        await event.save()
+        await ctx.reply('Дождитесь одобрения @tokutonariwa', {
+            reply_markup: new InlineKeyboard([[{
+                text: 'Approve',
+                callback_data: `approve:${event.id}`
+            }, {
+                text: 'Decline',
+                callback_data: `decline:${event.id}`
+            }]])
+        })
     })
 
-    events.command('full', async ctx => {
+    quoted.command('full', async ctx => {
         // TODO
     })
 
