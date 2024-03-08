08ee2a468ab5be9d8c2271268a2bd1e4975244b3
Dark Hole <dark.hole1@yandex.ru>
2024-03-08 11:32:10 +0300

    Switched back to brs

--- src/parts/brs.ts
+++ src/parts/brs.ts
@@ -20,4 +20,18 @@ export function brs(bot: Bot<ParseModeFlavor<Context>>) {
         })
         await writeFile('data/brs.json', JSON.stringify(brsFile))
     })
+
+    bot.on(':media', guard(isPrivateChat)).filter(
+        ctx => ctx.from?.id == DARK_HOLE,
+        async ctx => {
+            const photo = ctx.msg.photo
+            if (!photo) {
+                await ctx.reply('Чот странное', { reply_to_message_id: ctx.msg.message_id })
+                return
+            }
+            brsFile.queue.push(photo.slice(-1)[0].file_id)
+            await writeFile('data/brs.json', JSON.stringify(brsFile))
+            await ctx.reply(`Успешно добавлено на день ${brsFile.days + brsFile.queue.length}`, { reply_to_message_id: ctx.msg.message_id })
+        }
+    )
 }
