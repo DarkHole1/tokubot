534f14bce30ff5480b22524b8b7b5d6b3bd78bfb
Dark Hole <dark.hole1@yandex.ru>
2024-03-09 13:10:59 +0300

    Add authorizing wrap

---
+++ src/serverParts/authorize.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import { TokenDocument, TokenModel } from '../models/token'
+
+export type Authorized = (user: TokenDocument['user'], req: Request, res: Response) => unknown | Promise<unknown>
+export function authorize(f: Authorized) {
+    return async (req: Request, res: Response) => {
+        if (!req.query.token || typeof req.query.token != 'string') {
+            return res.json({
+                success: false,
+                error: {
+                    text: 'Unathorized'
+                }
+            })
+        }
+
+        const token = await TokenModel.findOne({ token: req.query.token })
+
+        if (!token) {
+            return res.json({
+                success: false,
+                error: {
+                    text: 'Unathorized'
+                }
+            })
+        }
+
+        return f(token.user, req, res)
+    }
+}
