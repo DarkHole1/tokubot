ff20b70431b8960fdb82560732b172be3c645e7c
Dark Hole <dark.hole1@yandex.ru>
2024-03-09 13:17:39 +0300

    Add token resolution

--- src/serverParts/whoami.ts
+++ src/serverParts/whoami.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express'
+import { tryAuthorize } from './authorize'
+import { TokenDocument } from '../models/token'
 
 export const whoami = Router()
 
-whoami.get('/whoami', (req, res) => {
+whoami.get('/whoami', async (req, res) => {
+    let token: TokenDocument | null = null
+    if(req.query.token && typeof req.query.token == 'string') {
+        token = await tryAuthorize(req.query.token)
+    }
+
     res.json({
         success: true,
-        data: {
+        data: token?.user ?? {
             id: 0,
             username: 'guest'
         }
