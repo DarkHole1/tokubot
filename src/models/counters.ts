cbb90888a22ba30621ae0c6c0a2e0c337f7bfb85
Dark Hole <dark.hole1@yandex.ru>
2024-03-17 22:01:28 +0300

    Add new everyday posting

--- src/models/counters.ts
+++ src/models/counters.ts
@@ -3,6 +3,9 @@ import { DocumentType, getModelForClass, prop } from '@typegoose/typegoose'
 class Counters {
     @prop({ default: 0 })
     worldTriggerDays!: number
+
+    @prop({ type: Number, default: () => new Map })
+    genericDays!: Map<string, number>
 }
 
 export type CountersDocument = DocumentType<Counters>
---
+++ src/parts/everyday-post.ts
@@ -0,0 +1,45 @@
+import { Bot } from 'grammy'
+import cron from 'node-cron'
+import { CountersModel } from '../models/counters'
+import { EverydayPostModel } from '../models/everyday-post'
+import { TOKU_CHAT } from '../constants'
+import debug from 'debug'
+
+const log = debug('tokubot:everyday-post')
+
+type Post = {
+    type: string,
+    text: string,
+    hours: number[]
+}
+
+const SCHEDULE: Post[] = [{
+    type: 'monogatari',
+    text: 'Irregular Monogatari Posting Day ???',
+    hours: [8]
+}]
+
+export function everydayPost(bot: Bot) {
+    cron.schedule('0 0 * * * *', async () => {
+        const counters = await CountersModel.findOne()
+        if (!counters) return
+        for (const post of SCHEDULE) {
+            const photo = await EverydayPostModel.findOne({ type: post.type })
+            if (!photo) {
+                return
+            }
+
+            let current = counters.genericDays.get(post.type) ?? 0
+            current++
+            try {
+                await bot.api.sendPhoto(TOKU_CHAT, photo.fileId, {
+                    caption: 'Постим World Trigger день ' + counters.worldTriggerDays
+                })
+            } catch (e) {
+                log(e)
+            }
+            await photo.deleteOne()
+        }
+        await counters.save()
+    })
+}
