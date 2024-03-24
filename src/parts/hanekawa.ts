6c362d55d7a84933ffdca999c829ee11afb240ca
Dark Hole <dark.hole1@yandex.ru>
2024-03-24 22:26:47 +0300

    Implement function

--- src/parts/hanekawa.ts
+++ src/parts/hanekawa.ts
@@ -1,12 +1,39 @@
 import { autoQuote } from '@roziscoding/grammy-autoquote'
 import { Composer } from 'grammy'
-import { ProfileModel } from '../models/profile'
+import { ProfileDocument, ProfileModel } from '../models/profile'
 
 export const hanekawa = new Composer().use(autoQuote)
 
 hanekawa.command('my', async ctx => {
-    if (ctx.match.length) {
+    if (ctx.match.length || !ctx.message?.from?.id) {
         await ctx.reply('Для использования напишите /my ссылка_на_ваш_аниме_профиль')
+        return
+    }
+
+    let match = ctx.match.match(/^https:\/\/shikimori\.(?:one|me)\/([^\/]+)$/)
+    if (match) {
+        const profile = await findOrCreate(ctx.message.from.id)
+        profile.shikimoriUsername = match[1]
+        await profile.save()
+        await ctx.reply('Добавила профиль на Шикимори')
+        return
+    }
+
+    match = ctx.match.match(/^https:\/\/anilist\.co\/user\/([^\/]+)\/$/)
+    if (match) {
+        const profile = await findOrCreate(ctx.message.from.id)
+        profile.anilistUsername = match[1]
+        await profile.save()
+        await ctx.reply('Добавила профиль на Анилисте')
+        return
+    }
+
+    match = ctx.match.match(/^https:\/\/myanimelist\.net\/profile\/([^\/]+)$/)
+    if (match) {
+        const profile = await findOrCreate(ctx.message.from.id)
+        profile.myanimelistUsernme = match[1]
+        await profile.save()
+        await ctx.reply('Добавила профиль на Мале')
         return
     }
 
@@ -14,7 +41,7 @@ hanekawa.command('my', async ctx => {
 })
 
 hanekawa.command('yours', async ctx => {
-    if(!ctx.message?.reply_to_message?.from?.id) {
+    if (!ctx.message?.reply_to_message?.from?.id) {
         await ctx.reply('Для использования напишите /yours в ответ на сообщение пользователя')
         return
     }
@@ -26,15 +53,25 @@ hanekawa.command('yours', async ctx => {
     }
 
     const results = [] as string[]
-    if(profile.shikimoriUsername) {
+    if (profile.shikimoriUsername) {
         results.push(`https://shikimori.one/${profile.shikimoriUsername}`)
     }
-    if(profile.anilistUsername) {
+    if (profile.anilistUsername) {
         results.push(`https://anilist.co/user/${profile.anilistUsername}/`)
     }
-    if(profile.myanimelistUsernme) {
+    if (profile.myanimelistUsernme) {
         results.push(`https://myanimelist.net/profile/${profile.myanimelistUsernme}`)
     }
 
     await ctx.reply(results.join('\n'))
 })
+
+async function findOrCreate(telegramID: number): Promise<ProfileDocument> {
+    const res = await ProfileModel.findOne({ telegramID })
+    if (res) {
+        return res
+    }
+    return new ProfileModel({
+        telegramID
+    })
+}
