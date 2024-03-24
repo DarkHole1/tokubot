96405c484902ae9ff3239c45c10db2a24283c936
Dark Hole <dark.hole1@yandex.ru>
2024-03-24 22:13:21 +0300

    Add sending user list

--- src/parts/hanekawa.ts
+++ src/parts/hanekawa.ts
@@ -1,5 +1,6 @@
 import { autoQuote } from '@roziscoding/grammy-autoquote'
 import { Composer } from 'grammy'
+import { ProfileModel } from '../models/profile'
 
 export const hanekawa = new Composer().use(autoQuote)
 
@@ -13,5 +14,27 @@ hanekawa.command('my', async ctx => {
 })
 
 hanekawa.command('yours', async ctx => {
-    // TODO
+    if(!ctx.message?.reply_to_message?.from?.id) {
+        await ctx.reply('Для использования напишите /yours в ответ на сообщение пользователя')
+        return
+    }
+
+    const profile = await ProfileModel.findOne({ telegramID: ctx.message.reply_to_message.from.id })
+    if (!profile || !(profile.shikimoriUsername && profile.anilistUsername && profile.myanimelistUsernme)) {
+        await ctx.reply('Я не знаю всего. Я знаю только то, что знаю.')
+        return
+    }
+
+    const results = [] as string[]
+    if(profile.shikimoriUsername) {
+        results.push(`https://shikimori.one/${profile.shikimoriUsername}`)
+    }
+    if(profile.anilistUsername) {
+        results.push(`https://anilist.co/user/${profile.anilistUsername}/`)
+    }
+    if(profile.shikimoriUsername) {
+        results.push(`https://shikimori.one/${profile.shikimoriUsername}`)
+    }
+
+    await ctx.reply(results.join('\n'))
 })
