a338209c61e461056f6aa352b239f9df760af7fa
Alexshy1337 <31924890+Alexshy1337@users.noreply.github.com>
2024-04-07 16:28:52 +0300

    fixing mistakes

--- src/parts/unspoil.ts
+++ src/parts/unspoil.ts
@@ -8,7 +8,7 @@ export const unspoil = new Composer().use(autoQuote)
 unspoil.command('unspoil', async ctx => {
     const reply = ctx.msg.reply_to_message
    //if (!reply || (!reply.photo && !reply.animation && !reply.video) || reply.has_media_spoiler) {
-    if (!reply) {
+    if (!reply || reply.has_media_spoiler) {//let's assume that if sender added spoiler to media they remember to add spoiler to text too. Or maybe we chould resend regardless of media spoiler on original message
         await ctx.reply('Для того чтобы убрать спойлер ответьте на сообщение')
         //await ctx.reply('Для того чтобы убрать спойлер ответьте на сообщение с кортинкой / гифкай')
         return
@@ -42,7 +42,11 @@ unspoil.command('unspoil', async ctx => {
             }
         )
     }
-    if (reply.text) {await ctx.reply(reply.from() + 'пишет: ||' + reply.text + '||')}
+    if (reply.text) {
+        await ctx.replyFmt(
+             fmt`${reply.from.username} пишет: ${spoiler(reply.text)}`
+        )
+    }
     try {
         await ctx.api.deleteMessage(reply.chat.id, reply.message_id)
     } catch (e) {
