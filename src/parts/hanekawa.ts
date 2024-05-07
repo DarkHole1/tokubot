035bd8f611fbd98192bc98144e91c346e6cb5d34
DarkHole1 <dark.hole1@yandex.ru>
2024-05-07 15:12:41 +0300

    Fixed bug with not diffirentiating between channels

--- src/parts/hanekawa.ts
+++ src/parts/hanekawa.ts
@@ -1,21 +1,22 @@
 import { autoQuote } from '@roziscoding/grammy-autoquote'
-import { Composer } from 'grammy'
+import { Composer, Context } from 'grammy'
 import { ProfileDocument, ProfileModel } from '../models/profile'
 import debug from 'debug'
+import { UserInfoFlavour } from './user-info'
 
 const log = debug('app:parts:hanekawa')
 
-export const hanekawa = new Composer().use(autoQuote)
+export const hanekawa = new Composer<UserInfoFlavour<Context>>().use(autoQuote)
 
 hanekawa.command('my', async ctx => {
-    if (ctx.match.length == 0 || !ctx.message?.from?.id) {
+    if (ctx.match.length == 0 || !ctx.userInfo.id) {
         await ctx.reply('Для использования напишите /my ссылка_на_ваш_аниме_профиль')
         return
     }
 
     let match = ctx.match.match(/^https:\/\/shikimori\.(?:one|me)\/([^\/]+)$/)
     if (match) {
-        const profile = await findOrCreate(ctx.message.from.id)
+        const profile = await findOrCreate(ctx.userInfo.id)
         profile.shikimoriUsername = match[1]
         await profile.save()
         await ctx.reply('Добавила профиль на Шикимори')
@@ -24,7 +25,7 @@ hanekawa.command('my', async ctx => {
 
     match = ctx.match.match(/^https:\/\/anilist\.co\/user\/([^\/]+)\/$/)
     if (match) {
-        const profile = await findOrCreate(ctx.message.from.id)
+        const profile = await findOrCreate(ctx.userInfo.id)
         profile.anilistUsername = match[1]
         await profile.save()
         await ctx.reply('Добавила профиль на Анилисте')
@@ -33,7 +34,7 @@ hanekawa.command('my', async ctx => {
 
     match = ctx.match.match(/^https:\/\/myanimelist\.net\/profile\/([^\/]+)$/)
     if (match) {
-        const profile = await findOrCreate(ctx.message.from.id)
+        const profile = await findOrCreate(ctx.userInfo.id)
         profile.myanimelistUsernme = match[1]
         await profile.save()
         await ctx.reply('Добавила профиль на Мале')
