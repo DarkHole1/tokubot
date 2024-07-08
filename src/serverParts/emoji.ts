f4c2bd6d1b4ffb3cb74ad7b86e300204778c652a
DarkHole1 <dark.hole1@yandex.ru>
2024-07-08 12:58:19 +0300

    Add api for rating

--- src/serverParts/emoji.ts
+++ src/serverParts/emoji.ts
@@ -3,6 +3,23 @@ import { getCurrentCounter } from "../parts/emoji-counter"
 
 export const emoji = Router()
 
+emoji.get('/emoji/rating', async (_req, res) => {
+    const counter = await getCurrentCounter()
+
+    if (!counter) {
+        return res.json([])
+    }
+
+    const sum = (a: Iterable<number>) => Array.from(a).reduce((a, b) => a + b)
+
+    const countByUser = Array.from(counter.byUser.values()).map((counter) => ({
+        name: counter.name, 
+        count: sum(counter.counters.values())
+    }))
+
+    return res.json(countByUser.sort((a, b) => b.count - a.count))
+})
+
 emoji.get('/emoji', async (req, res) => {
     if (typeof req.query.id != 'string') {
         return res.json({})
