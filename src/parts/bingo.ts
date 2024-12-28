63ac839902b899d8a2e1ad3e4730921966873d79
DarkHole1 <dark.hole1@yandex.ru>
2024-12-29 00:50:33 +0300

    Make new bingo link

--- src/parts/bingo.ts
+++ src/parts/bingo.ts
@@ -4,4 +4,4 @@ import { autoQuote } from "@roziscoding/grammy-autoquote"
 export const bingo = new Composer
 const quoted = bingo.use(autoQuote)
 
-quoted.command('bingo', ctx => ctx.reply('https://docs.google.com/spreadsheets/d/1QghSAYndgtDBDYPjEO1FNsMsP765zHK-wAFDF9Q1h9Y/edit#gid=0'))
+quoted.command('bingo', ctx => ctx.reply('https://docs.google.com/spreadsheets/d/1DErtjsrgaIxo1HvB_ETu6v3H-d43RkOItJ284nm7H4Y/edit?usp=sharing'))
