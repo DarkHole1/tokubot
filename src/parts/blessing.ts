820c62f39da76b1ff9a6d79019a08fdc33da3e95
DarkHole1 <dark.hole1@yandex.ru>
2025-02-21 13:16:58 +0300

    Add new blessing

--- src/parts/blessing.ts
+++ src/parts/blessing.ts
@@ -1,11 +1,19 @@
 import { autoQuote } from '@roziscoding/grammy-autoquote'
 import { Composer } from 'grammy'
+import { LIGHTING } from '../constants'
 
 export const blessing = new Composer
 const quoted = blessing.use(autoQuote)
 
 // Похвала
-quoted.on('msg').filter(_ => Math.random() > 0.998, ctx => ctx.reply('Ты умничка'))
+quoted.on('msg').filter(_ => Math.random() > 0.998, async ctx => {
+    const hours = new Date().getHours()
+    if (hours >= 0 && hours <= 6 && Math.random() > 0.7) {
+        await ctx.replyWithPhoto(LIGHTING)
+        return
+    }
+    await ctx.reply('Ты умничка')
+})
 
 // Фрирен
 quoted.on('msg').filter(_ => Math.random() > 0.99999, ctx => ctx.reply('Фрирен'))
