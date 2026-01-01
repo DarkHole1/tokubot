057f83638ded2c0ac91f32874230cb1e294af82f
Dark Hole <dark.hole1@yandex.ru>
2026-01-01 17:30:59 +0300

    New year, new bingo

--- src/parts/bingo.ts
+++ src/parts/bingo.ts
@@ -4,4 +4,4 @@ import { autoQuote } from "@roziscoding/grammy-autoquote"
 export const bingo = new Composer
 const quoted = bingo.use(autoQuote)
 
-quoted.command('bingo', ctx => ctx.reply('https://docs.google.com/spreadsheets/d/1DErtjsrgaIxo1HvB_ETu6v3H-d43RkOItJ284nm7H4Y/edit?usp=sharing'))
+quoted.command('bingo', ctx => ctx.reply('https://docs.google.com/spreadsheets/d/1znxn6x-5W7qyHz8BBWFT3mpU58ibYj8cs1jKsCNo_10/edit?usp=sharing'))
